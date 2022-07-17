d6eb1e1e9a68c3cb0ae388c61a3cb2bb4e037e97
David(gecert) <dkarandze4@gmail.com>
2022-07-17 13:24:08 +0200

    Renamed project

--- django_catalog/urls.py
+++ django_catalog/urls.py
@@ -1,4 +1,4 @@
-"""catalog URL Configuration
+"""django_catalog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
